989e9ac80d739286bd6c5e59641099d3191b2f5a
August Zhu <Augustzhu@139.com>
2017-04-13 09:36:02 +0800

    fixed  some bugs

--- src/Day03/people.java
+++ src/Day03/people.java
@@ -5,34 +5,38 @@ package Day03;
  *
  */
 public class people {
-private String name;
-private String sex;
-private int age;
-private double weight;
+	private String name;
+	private String sex;
+	private int age;
+	private double weight;
 
- public  people (String jname,String jsex,int jage, double jweight){
-	 name=jname;
-	 sex=jsex;
-	 age=jage;
-	 weight=jweight;
- }
-   public String eat (String food){
-	 return "我在吃"+food ;
- }
-   void print(String a){
-	   System.out.println(a);
-   }
- public void game(){
-	 System.out.println("我在打游戏");
- }
- public static void main(String[] args){
-	 people people= new people ("李明","男",21,65);
-	 System.out.println("姓名"+people.name);
-	 System.out.println("姓名"+people.sex);
-	 System.out.println("姓名"+people.age);
-	 System.out.println("姓名"+people.weight);
-	 people.game();
-	 String food="可口的米饭";
-	 people.print(people.eat(food));
- }
- }
+	public people(String jname, String jsex, int jage, double jweight) {
+		name = jname;
+		sex = jsex;
+		age = jage;
+		weight = jweight;
+	}
+
+	public String eat(String food) {
+		return "我在吃" + food;
+	}
+
+	void print(String a) {
+		System.out.println(a);
+	}
+
+	public void game() {
+		System.out.println("我在打游戏");
+	}
+
+	public static void main(String[] args) {
+		people people = new people("李明", "男", 21, 65);
+		System.out.println("姓名" + people.name);
+		System.out.println("姓名" + people.sex);
+		System.out.println("姓名" + people.age);
+		System.out.println("姓名" + people.weight);
+		people.game();
+		String food = "可口的米饭";
+		people.print(people.eat(food));
+	}
+}
