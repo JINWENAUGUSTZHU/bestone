822865119908da92e166333ddb912939a420b7ab
August Zhu <Augustzhu@139.com>
2017-04-13 08:36:27 +0800

    add people.java

---
+++ src/Day03/people.java
@@ -0,0 +1,32 @@
+package Day03;
+
+/**
+ * @author August Zhu
+ *
+ */
+public class people {
+private String name;
+private String sex;
+private int age;
+private double weight;
+
+ public  people (String jname,String jsex,int jage, double jweight){
+	 name=jname;
+	 sex=jsex;
+	 age=jage;
+	 weight=jweight;
+ }
+ public String eat (String food){
+	 return "我在吃"+food;
+ }
+ public void game(){
+	 System.out.println("我在打游戏");
+ }
+ public static void main(String[] args){
+	 people people= new people ("李明","男",21,65);
+	 System.out.println("姓名"+people.name);
+	 System.out.println("姓名"+people.sex);
+	 System.out.println("姓名"+people.age);
+	 System.out.println("姓名"+people.weight);
+ }
+ }
