e5621929a403edfb8623e608c7bd448fcf576b3d
August Zhu <Augustzhu@139.com>
2017-05-07 12:35:15 +0800

    some interesting

---
+++ src/Day15/first.java
@@ -0,0 +1,16 @@
+package Day15;
+
+/**
+ * @author August Zhu
+ *
+ */
+public class first {
+	public static void main (String [] args){
+		for (int i=1;i<5;i++){
+			i++;
+			System.out.println(i);
+		}
+		
+	}
+
+}
